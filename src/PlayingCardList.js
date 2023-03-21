633f91a2a19faa3e4ca3c24779692be62fd3a4a2
henry <nguyentuanhoang1994@yahoo.com>
2023-03-21 00:01:42 -0700

    init

---
+++ src/PlayingCardList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useAxios } from "./hooks";
+import PlayingCard from "./PlayingCard";
+import { formatCard } from "./helpers";
+import "./PlayingCardList.css";
+
+/* Renders a list of playing cards.
+ * Can also add a new card at random,
+remove all cards */
+function CardTable() {
+  const [ cards, addCard, clearCards ] = useAxios(
+    "cards",
+    "https://deckofcardsapi.com/api/deck/new/draw/"
+  );
+
+  // const addCard = async () => {
+  //   const response = await axios.get(
+  //     "https://deckofcardsapi.com/api/deck/new/draw/"
+  //   );
+  //   setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+  // };
+  return (
+    <div className="PlayingCardList">
+      <h3>Pick a card, any card!</h3>
+      <div>
+        <button onClick={() => addCard(formatCard)}>Add a playing card!</button>
+        <button onClick={clearCards}>Clear the table</button>
+      </div>
+      <div className="PlayingCardList-card-area">
+        {cards.map(card => (
+          <PlayingCard key={card.id} front={card.image} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+CardTable.defaultProps = {};
+
+export default CardTable;
